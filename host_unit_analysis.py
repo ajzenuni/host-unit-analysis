2bd0de65cb1034b2a99567037abfce58eab73d9f
ajzenuni <arijan.zenuni@dynatrace.com>
2024-03-13 14:05:59 -0700

    fix: next page iteration

--- host_unit_analysis.py
+++ host_unit_analysis.py
@@ -3,19 +3,19 @@ import requests
 import json
 from helper.api import check_error
 from helper.host_unit import calculate_host_unit
-import schedule
 import time
 
-def get_host(url, headers, host_groups):
+def get_host(url, headers, host_groups, fromTime):
     host_url = f'{url}/api/v2/entities'
     init_params = {
         'pageSize': 1000,
         'entitySelector': 'type("host")',
-        'from': 'now-1d',
+        'from': fromTime,
         'fields': 'properties.monitoringMode,properties.memoryTotal,fromRelationships.isInstanceOf'
     } 
-    while host_url:
-        response = requests.get(host_url, headers=headers, params=init_params)
+    go = True
+    while go:
+        response = requests.get(host_url, headers=headers, params=init_params, verify=False)
         data = response.json()
         check_error(data)
         for entity in data['entities']:
@@ -41,22 +41,27 @@ def get_host(url, headers, host_groups):
                     "hostUnit": host_unit
                 })
                 host_groups["no_host_group"]["totalHostUnit"] += host_unit
-        host_url = data.get('nextPageKey')
+        init_params = {
+            'nextPageKey': data.get('nextPageKey')
+        }
+        if(data.get('nextPageKey') == None):
+            go = False
         time.sleep(0.500)
 
     return host_groups
 
-def get_host_group(url, headers):
+def get_host_group(url, headers,fromTime):
     host_group_url = f'{url}/api/v2/entities'
     init_params = {
         'pageSize': 1000,
         'entitySelector': 'type("host_group")',
-        'from': 'now-1d'
+        'from': fromTime
     }
     host_groups = {'no_host_group':{'displayName':'none','hosts':[],'totalHostUnit':0}}
 
-    while host_group_url:
-        response = requests.get(host_group_url, headers=headers, params=init_params)
+    go = True
+    while go:
+        response = requests.get(host_group_url, headers=headers, params=init_params, verify=False)
         data = response.json()
         check_error(data)
         for entity in data['entities']:
@@ -64,7 +69,11 @@ def get_host_group(url, headers):
             display_name = entity['displayName']
             host_groups[entity_id] = {'displayName': display_name, "hosts":[], "totalHostUnit":0}
 
-        host_group_url = data.get('nextPageKey')
+        init_params = {
+            'nextPageKey': data.get('nextPageKey')
+        }
+        if(data.get('nextPageKey') == None):
+            go = False
         time.sleep(0.500)
 
     return host_groups
@@ -80,19 +89,20 @@ def post_host_unit_analysis_metric(host_group_analysis_final, url, headers):
         metric_lines.append(f'hostUnit.analysis,host_group.name="{display_name}" {total_host_unit:.2f}')
     
     lines = '\n'.join(metric_lines)
-    response = requests.post(metric_url,headers=headers,data=lines.encode('utf-8'))
+    response = requests.post(metric_url,headers=headers,data=lines.encode('utf-8'), verify=False)
     data = response.json()
     check_error(data)
     print(data)
 
 def main():
-    if len(sys.argv) != 5:
-            print("Usage: python host_unit_analysis.py managed_url managed_api-token(API v2:Read Entities,API v1:Access problem and event feed, metrics, and topology) saas_url saas_api-token(API v2:Ingest metrics)")
+    if len(sys.argv) != 6:
+            print("Usage: python host_unit_analysis.py managed_url managed_api-token(API v2:Read Entities,API v1:Access problem and event feed, metrics, and topology) saas_url saas_api-token(API v2:Ingest metrics) fromTime")
             sys.exit(1)
     urlGet = sys.argv[1].rstrip('/')
     tokenGet = sys.argv[2]
     urlPost = sys.argv[3].rstrip('/')
     tokenPost = sys.argv[4]
+    fromTime = sys.argv[5]
 
     headersGet = {
         'Content-Type': 'application/json', 
@@ -104,21 +114,21 @@ def main():
         "charset": 'utf-8',
         'Authorization' : f'Api-Token {tokenPost}'
     }
-    # Define a job function that runs your script with provided arguments
-    def job():
-        print("Job is running...")
-        host_groups = get_host_group(urlGet, headersGet)
-        final_host_groups = get_host(urlGet, headersGet, host_groups)
-        post_host_unit_analysis_metric(final_host_groups, urlPost, headersPost)
-        print("Job completed successfully.")
+    # # Define a job function that runs your script with provided arguments
+    # def job():
+    print("Job is running...")
+    host_groups = get_host_group(urlGet, headersGet,fromTime)
+    final_host_groups = get_host(urlGet, headersGet, host_groups,fromTime)
+    post_host_unit_analysis_metric(final_host_groups, urlPost, headersPost)
+    print("Job completed successfully.")
 
-    # Schedule the job to run every 2 minutes
-    schedule.every(2).minutes.do(job)
+    # # Schedule the job to run every 2 minutes
+    # schedule.every(2).minutes.do(job)
 
-    # Main loop to continuously check and run scheduled jobs
-    while True:
-        schedule.run_pending()
-        time.sleep(1)  # Sleep for a second to avoid high CPU usage
+    # # Main loop to continuously check and run scheduled jobs
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)  # Sleep for a second to avoid high CPU usage
 
 if __name__ == '__main__':
     main()
